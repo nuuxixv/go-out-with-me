af46ad939f2d3fb86d54a868f9d4cf9327fc442d
Your <your_email@example.com>
2026-02-07 04:38:52 +0900

    "2"

--- components/KakaoScript.tsx
+++ components/KakaoScript.tsx
@@ -2,24 +2,24 @@
 
 import Script from "next/script";
 
+declare global {
+    interface Window {
+        Kakao: any;
+    }
+}
+
 export default function KakaoScript() {
     const onLoad = () => {
-        if ((window as any).Kakao && !(window as any).Kakao.isInitialized()) {
+        if (window.Kakao && !window.Kakao.isInitialized()) {
             const kakaoKey = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
             if (kakaoKey) {
-                (window as any).Kakao.init(kakaoKey);
+                window.Kakao.init(kakaoKey);
                 console.log("Kakao SDK Initialized");
             }
         }
     };
 
     return (
-        <Script
-            src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
-            integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2txfDWp1Ps941/+NC7Ubt+"
-            crossOrigin="anonymous"
-            strategy="afterInteractive" // Load immediately after hydration
-            onLoad={onLoad}
-        />
+        <script src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.7/kakao.min.js" integrity="sha384-tJkjbtDbvoxO+diRuDtwRO9JXR7pjWnfjfRn5ePUpl7e7RJCxKCwwnfqUAdXh53p" crossOrigin="anonymous"></script>
     );
 }
